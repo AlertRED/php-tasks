dd7362d82e2f9f09677e88832ffa54d4930a02b7
unknown <ieaiaio@bk.ru>
2019-03-05 23:25:21 +0300

    add migration github_user

---
+++ database/migrations/2019_03_05_201218_create_github_users_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGithubUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('github_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('github_users');
+    }
+}
